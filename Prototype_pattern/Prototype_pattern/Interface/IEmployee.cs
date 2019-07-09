5e20e4bfcdfd0fa929816ab40e1fe3c676192239
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2019-07-09 11:56:11 +0600

    add prototype-design-pattern

---
+++ Prototype_pattern/Prototype_pattern/Interface/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_pattern.Interface
+{
+    public interface IEmployee
+    {
+        IEmployee Clone();
+        string GetDetails();
+    }
+}
